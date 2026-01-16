2050317af656fd1340a741239b5d8798c8492f81
Akash Kush <138812613+Akash9634@users.noreply.github.com>
2026-01-16 18:43:07 +0530

    Create Fibonacci.java

---
+++ basics/Fibonacci.java
@@ -0,0 +1,29 @@
+// Q. Fibonacci series
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int n1 = 0;
+        int n2 = 1;
+        int n3 = 0;
+        
+        if(n==1){
+            System.out.println(n1);
+        }
+        else if(n==2){
+            System.out.println(n2);
+        }
+        else{
+        for(int i=3; i<=n; i++){
+            n3 = n1 + n2;
+            n1=n2;
+            n2=n3;
+            System.out.println(n3);
+        }
+        
+    }
+        
+    }
+}
