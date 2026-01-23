355e81756600b7f6943e1e52674315dab9cbd3a0
Akash Kush <138812613+Akash9634@users.noreply.github.com>
2026-01-23 20:27:34 +0530

    Implement basic LinkedList functionality

--- basics/Linked_List_Basics.java
+++ basics/Linked_List_Basics.java
@@ -1,1 +1,146 @@
+import java.util.*;
+
+public class Main {
+  public static class Node {
+    int data;
+    Node next;
+
+  }
+
+  public static class LinkedList {
+    Node head;
+    Node tail;
+    int size;
+
+    void addLast(int val) {
+      Node temp = new Node();
+      temp.data = val;
+      temp.next = null;
+      if (size == 0) {
+        head = tail = temp; // because linked list is empty
+        size++;
+      } else {
+        tail.next = temp;
+        tail = temp;
+        size++;
+      }
+    }
+
+    void display() {
+      Node temp = head;
+      for (int i = 0; i < size; i++) {
+        System.out.println(temp.data);
+        temp = temp.next;
+      }
+    }
+
+    void size() {
+      Node temp = head;
+      int length = 0;
+
+      while (temp != null) {
+        length++;
+        temp = temp.next;
+      }
+      System.out.println(length);
+    }
+
+    void removeFirst(){
+      if(head==null){
+        System.out.println("list is empty");
+      }
+      else if(size==1){
+        head = null;
+        tail = null;
+        size = 0;
+      }
+      else{
+        head = head.next;
+        size--;
+      }
+    }
+
+    void getFirst(){
+      System.out.println(head.data);
+    }
+
+    void getLast(){
+      System.out.println(tail.data);
+    }
+
+    //0 based search
+    void getAt(int idx){
+      Node temp = head;
+      if(head==null || idx<0 || size<=idx){
+        System.out.println("-1");
+      }
+
+      else if(idx==0){
+        System.out.println(temp.data);
+      }
+      else{
+        for(int i = 0; i<idx; i++){
+          temp=temp.next;
+        }
+        System.out.println(temp.data);
+    }
+      
+    }
+
+    void addFirst(int val){
+      Node newNode = new Node();
+      newNode.data = val;
+      newNode.next = head;
+      head = newNode;
+      if(size==0){
+        tail = newNode;
+      }
+      
+      size++;
+    }
+
+    // void addAtIndex(int idx){
+    //   if(size==0){
+    //     addFirst(idx);
+    //   }
+    //   for(int i=0; i<idx; i++){
+
+    //   }
+    // }
+
+  }
+
+  public static void main(String[] args) {
+    LinkedList ll = new LinkedList();
+    ll.addLast(5);
+    ll.addLast(6);
+    ll.addLast(8);
+    ll.addLast(10);
+
+
+    //  ll.removeFirst();
+
+    
+
+    System.out.println("first element is: ");
+    ll.getFirst();
+
+
+    System.out.println("element is: ");
+    ll.getAt(1);
+
+    // ll.size();
+    ll.addFirst(67);
+    System.err.println();
+    System.out.println();
+    ll.display();
+
+    ll.size();
+
+   
+    // System.out.println("done");
+
+  }
+
+}
 
